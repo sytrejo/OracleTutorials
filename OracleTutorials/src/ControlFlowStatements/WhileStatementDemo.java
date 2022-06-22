ab3c4168d390df8c2f7912d736386204869d3a75
Silvia Trejo <s.y.trejo26@gmail.com>
2022-06-22 18:24:14 -0400

    Updating files

---
+++ OracleTutorials/src/ControlFlowStatements/WhileStatementDemo.java
@@ -0,0 +1,15 @@
+package ControlFlowStatements;
+
+public class WhileStatementDemo {
+	
+	
+	    public static void main(String[] args){
+	        int count = 1;
+	        while (count < 11) {
+	            System.out.println("Count is: " + count);
+	            count++;
+	        }
+	    }
+}
+
+
